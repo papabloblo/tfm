38a6b1055b0d96493cc43792375ab1d62bd41a09
Pablo Hidalgo <pablohigar@gmail.com>
2020-08-25 17:33:14 +0200

    Borrador

---
+++ src/old/mixed_01.py
@@ -0,0 +1,16 @@
+from src.VNS2 import VNS
+from src.WasteCollection import WasteCollection
+
+
+w_mixed = WasteCollection(file_filling_rates="data/pickup-point-filling-rates-mixed.csv",
+                          file_times="data/times-between-pickup-points.txt")
+
+collection_mixed = RouteCollection(w_mixed,
+                                   orig=5,
+                                   dest=5,
+                                   horizon=5,
+                                   max_tabu=0
+                                   )
+
+opt_mixed = VNS(collection_mixed, path='results/mixed_01')
+opt_mixed.GVNS(2,2,1)
